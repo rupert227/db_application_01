659f460119b16a4d8cb29d3dd20375e000339c33
rupert tabilin <rupert.tabilin.ed@outlook.com>
2025-03-25 17:40:40 +0800

    added and renamed Files

--- CCINFOM S14-1-sql.sql
+++ CCINFOM S14-1-sql.sql
@@ -60,15 +60,15 @@ INSERT INTO refRoomTypes (roomType, roomPrice, maxCapacity) VALUES
 -- Insert into roomRecords
 INSERT INTO roomRecords (roomType, availability) VALUES
 ('Standard', 'Available'),
-('Standard', 'Occupied'),
+('Standard', 'Available'),
 ('Standard', 'Available'),
 ('Double', 'Available'),
-('Double', 'Occupied'),
 ('Double', 'Available'),
-('Suite', 'Occupied'),
+('Double', 'Available'),
 ('Suite', 'Available'),
 ('Suite', 'Available'),
-('Suite', 'Occupied');
+('Suite', 'Available'),
+('Suite', 'Available');
 
 -- Insert into guestRecords
 INSERT INTO guestRecords (firstName, lastName, email, telNo) VALUES
@@ -86,19 +86,19 @@ INSERT INTO guestRecords (firstName, lastName, email, telNo) VALUES
 -- Insert into reservationRecords
 INSERT INTO reservationRecords (bookRefID, roomRefID, guestCount, checkInDate, checkOutDate, reservationStatus) VALUES
 (1, 2, 1, '2025-03-10 14:00:00', '2025-03-12 10:00:00', 'Checked-out'),
-(2, 4, 2, '2025-03-15 14:00:00', '2025-03-18 10:00:00', 'Checked-in'),
-(3, 6, 2, '2025-03-17 14:00:00', '2025-03-19 10:00:00', 'Confirmed'),
+(2, 4, 2, '2025-03-15 14:00:00', '2025-03-18 10:00:00', 'Checked-out'),
+(3, 6, 2, '2025-03-17 14:00:00', '2025-03-19 10:00:00', 'Checked-out'),
 (4, 7, 4, '2025-03-12 14:00:00', '2025-03-16 10:00:00', 'Checked-out'),
-(5, 8, 3, '2025-03-20 14:00:00', '2025-03-23 10:00:00', 'Confirmed'),
+(5, 8, 3, '2025-03-20 14:00:00', '2025-03-23 10:00:00', 'Checked-out'),
 (6, 9, 1, '2025-03-11 14:00:00', '2025-03-14 10:00:00', 'Cancelled'),
-(7, 3, 1, '2025-03-18 14:00:00', '2025-03-21 10:00:00', 'Confirmed'),
+(7, 3, 1, '2025-03-18 14:00:00', '2025-03-21 10:00:00', 'Checked-out'),
 (8, 5, 2, '2025-03-14 14:00:00', '2025-03-16 10:00:00', 'Checked-out'),
-(9, 10, 4, '2025-03-22 14:00:00', '2025-03-25 10:00:00', 'Confirmed'),
+(9, 10, 4, '2025-03-22 14:00:00', '2025-03-25 10:00:00', 'Checked-out'),
 (10, 1, 1, '2025-03-09 14:00:00', '2025-03-11 10:00:00', 'Checked-out');
 
 -- Insert into invoiceRecords
 INSERT INTO invoiceRecords (reserveID, bookRefID, paymentStatus, totalCost, paymentMethod, paymentDate) VALUES
-(1, 1, 'Paid', 160.00, 'Credit Card', '2025-03-10 16:00:00'),
+(1, 1,'Paid', 160.00, 'Credit Card', '2025-03-10 16:00:00'),
 (2, 2, 'Paid', 360.00, 'Debit Card', '2025-03-15 16:00:00'),
 (3, 3, 'Paid', 240.00, 'Cash', '2025-03-17 16:00:00'),
 (4, 4, 'Paid', 800.00, 'Credit Card', '2025-03-12 16:00:00'),
